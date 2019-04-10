d4e0093912282e9864f5efe46e3e8e0973a177a8
Sileshi Chukala <sileshic@gmail.com>
2019-04-11 01:43:53 +0200

    final version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const products = require('./public/assets/js/products.json');
-const mathl = require('./private/mathLogic');
+const mathl = require('./private/calculateSum');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -9,8 +9,21 @@ app.listen(process.env.port || 489);
 console.log('Running at Port 489');
 console.log(products);
 
-router.get('/calculate/', function (res) {
-    res.send(mathl.showTotalPrice());
+router.post('/calculate', function (req, res) {
+    // req.body == cart object
+    let calculatedPrice = mathl.calcTotal(req.body);
+
+    let sendObject = {
+        "Total": calculatedPrice
+    };
+
+    res.send(JSON.stringify(sendObject));
 });
+
+app.use(express.json());
 app.use(express.static("public"));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use('/', router);
+ 
--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,24 @@
 let gulp = require('gulp');
 const minify = require('gulp-minify');
 const htmlmin = require('gulp-htmlmin');
-const changed = require('gulp-changed');
-const ngAnnotate = require('gulp-ng-annotate');
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant'); 
-gulp.task('default', function () {
-    return;
+const imagemin = require('gulp-imagemin');
+
+gulp.task('compressCustom', () => {
+    return gulp.src(['./public/assets/js/custom.js']).pipe(minify()).pipe(gulp.dest('dist/custom'))
+});
+gulp.task('compressImages', () =>
+    gulp.src(['./public/assets/images/*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+gulp.task('compressAll', () => {
+    return gulp.src(['./public/assets/js/*.js']).pipe(minify()).pipe(gulp.dest('dist/all'))
 });
 gulp.task('compress', () => {
-    return gulp.src(['./public/assets/js/custom.js']).pipe(minify()).pipe(gulp.dest('dist/custom'));
-});
-gulp.task('compressAll', () => {
-    return gulp.src(['./public/assets/js/*.js']).pipe(minify()).pipe(gulp.dest('dist/all'));
+    return gulp.src(['./private/*.js']).pipe(minify()).pipe(gulp.dest('dist/all'))
 });
 gulp.task('html', () => {
-    return gulp.src('./public/index.html').pipe(htmlmin({collapseWhitespace: true
+    return gulp.src('./public/thank.html').pipe(htmlmin({
+        collapseWhitespace: true
     })).pipe(gulp.dest('dist/html'));
 });
-
-const SRC = 'src/*.js';
-const DEST = 'dist';
-gulp.task('default', () =>
-    gulp.src(SRC)
-    .pipe(changed(DEST))
-    // `ngAnnotate` will only get the files that
-    // changed since the last time it was run
-    .pipe(ngAnnotate())
-    .pipe(gulp.dest(DEST))
-);
-
-
-(async () => {
-    const files = await imagemin(['images/*.{jpg,png}'], 'build/images', {
-        plugins: [imageminJpegtran(),imageminPngquant({quality: '65-80'})]
-    });
-})();
